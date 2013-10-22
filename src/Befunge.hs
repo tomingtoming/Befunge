58d147db3f9eb0c325c51ea41cc913f912d61aae
toming <tomingtoming@gmail.com>
2013-10-22 14:42:38 +0900

    Befunge

--- src/Befunge.hs
+++ src/Befunge.hs
@@ -1,6 +1,15 @@
 module Befunge where
 
 import Befunge.Field
+import Befunge.Stack
 
 class Befunge b where
-  step :: Field f => b -> f -> IO Bool
+  step     :: Field f => b -> f -> IO Bool
+  getX     :: b -> IO Int
+  getY     :: b -> IO Int
+
+steps :: (Befunge b, Field f) => b -> f -> IO ()
+steps bf f = do
+  alive <- step bf f
+  if alive then steps bf f
+           else return ()
